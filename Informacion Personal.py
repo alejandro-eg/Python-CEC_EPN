0b509442d8cd3f77f178356c41d97c37b7f1d256
Alejandro Egas <alejoegasg@outlook.com>
2022-08-02 21:13:17 -0500

    Informacion Personal

---
+++ Informacion Personal.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
+edad = str(input("Ingrese su edad: "))
+ciudad = input("Ingrese la ciudad donde vive: ")
+space= " "
+
+print("Nombre: "+nombre+space+"Apellido: "+apellido+space+"Edad: "
++edad+space+"Ciudad: "+ciudad)
