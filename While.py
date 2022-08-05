82849171c558841264c7477875c7704727f0747f
Alejandro Egas <alejoegasg@outlook.com>
2022-08-04 22:26:15 -0500

    While

---
+++ While.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+contar=input("Ingrese el # hasta el que quiere contar")
+contar=int(contar)
+contador=1
+
+while contador <=contar:
+    print(contador ,end=" ")
+    contador+=1
