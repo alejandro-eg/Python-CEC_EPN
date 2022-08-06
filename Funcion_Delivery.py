8a506edb9f857c7eb74911fdda5b149fe205cc84
Alejandro Egas <alejoegasg@outlook.com>
2022-08-05 22:06:33 -0500

    Funcion_Delivery

---
+++ Funcion_Delivery.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+def delivery (barrio,calle,casa):
+    print("La direccion de la entrega de su pedido es: ",barrio,calle,casa)
+barrio=input("Ingrese el nombre del barrio: ")
+calle=input("Ingrese el nombre de la calle: ")
+casa=input("Ingrese el número de casa: ")
+delivery(barrio, calle, casa)
