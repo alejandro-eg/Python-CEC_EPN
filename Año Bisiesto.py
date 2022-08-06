c8d90bce787fca86149eeb9c22c5d5fdfe7b1665
Alejandro Egas <alejoegasg@outlook.com>
2022-08-06 12:57:50 -0500

    Año Bisiesto

---
+++ Año Bisiesto.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+def es_bisiesto(anio):
+    if anio % 400 == 0:
+        return True
+    elif anio % 100 == 0:
+        return False
+    elif anio % 4 == 0:
+        return True
+    else:
+        return False
+testData = [1900, 2000, 2016, 1987]
+testResults = [False, True, True, False]
+for i in range(len(testData)):
+	yr = testData[i]
+	print(yr,"->",end="")
+	result =es_bisiesto(yr)
+	if result == testResults[i]:
+		print("OK")
+	else:
+		print("Failed")
